07f0cc2feaca5b468038a552c214f85c8881cb99
Matt Watson <mattwat@gmail.com>
2020-06-17 15:02:14 -0400

    setup minimal linking example

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,9 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.createSchemaCustomization = ({ actions }) => {
+    const { createTypes } = actions
+    const typeDefs = `
+        type PeopleYaml implements Node {
+            teams: [TeamsYaml] @link(by: "members.id", from: "id")
+        }
+    `
+    createTypes(typeDefs)
+}
